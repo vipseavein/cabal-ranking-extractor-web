57ebfbe5cb26d6b4ecd829af1c50441271b871a1
vipseavein <bladerphong@gmail.com>
2026-09-21 11:49:02 +0700

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='cabal-extractor-web-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
