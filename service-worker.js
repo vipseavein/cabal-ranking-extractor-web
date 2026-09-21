daa4e587720ccf52b0ae4e41cfa442b43b13cd7d
vipseavein <bladerphong@gmail.com>
2026-09-21 12:10:57 +0700

    Bump cache to v1.0.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cabal-extractor-web-v1.0.1';
+const CACHE='cabal-extractor-web-v1.0.2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
